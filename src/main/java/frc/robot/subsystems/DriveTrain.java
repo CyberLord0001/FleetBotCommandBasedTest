9052a40d404a1d0f55d7908518189f01fc91391f
CyberLord0001 <lucashshen@gmail.com>
2024-01-23 10:03:41 -0700

    Implemented CANSparkMax and WPI_Talon FX in the Drive Train and Shooter respectively

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,16 +4,18 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveTrain extends SubsystemBase {
-  private Talon m_left1 = new Talon(0);
-  private Talon m_left2 = new Talon(1);
-  private Talon m_right1 = new Talon(2);
-  private Talon m_right2 = new Talon(3);
+  private CANSparkMax m_left1 = new CANSparkMax(0, MotorType.kBrushless);
+  private CANSparkMax m_left2 = new CANSparkMax(1, MotorType.kBrushless);
+  private CANSparkMax m_right1 = new CANSparkMax(2, MotorType.kBrushless);
+  private CANSparkMax m_right2 = new CANSparkMax(3, MotorType.kBrushless);
   private MotorControllerGroup m_leftMotors = new MotorControllerGroup(m_left1, m_left2);
   private MotorControllerGroup m_rightMotors = new MotorControllerGroup(m_right1, m_right2);
   private DifferentialDrive m_differentialDrive = new DifferentialDrive(m_leftMotors, m_rightMotors);
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,17 +4,17 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
-    private Talon m_shooter1 = new Talon(4);
-    private Talon m_shooter2 = new Talon(5);
+    private WPI_TalonFX m_shooter1 = new WPI_TalonFX(4);
+    private WPI_TalonFX m_shooter2 = new WPI_TalonFX(5);
     private MotorControllerGroup m_shooters = new MotorControllerGroup(m_shooter1, m_shooter2);
   /** Creates a new DriveTrain. */
   public Shooter() {}
-  
+
   public void shoot(){
     m_shooters.set(0.65);
   }
